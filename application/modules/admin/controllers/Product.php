7b6ab8bc3dc94805ea4ccf76e2bca4c5303eeee9
josechanfm <josechan@ipm.edu.mo>
2019-07-04 15:15:15 +0800

    Shopping cart with product list
    Add products table
    Create folder under \assets\products\images\ with sample images
    Ajax shopping cart, \assets\dist\admin\jquery.mycart.js

---
+++ application/modules/admin/controllers/Product.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product extends Admin_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$crud = $this->generate_crud('products');
+		$crud->columns('sub_category','brand','name','origin','image');
+		//$crud->field_type('category', 'dropdown', $this->config->item('product_category'));
+		//$crud->set_relation('sub_category','categories','name');
+		$crud->set_field_upload('image','assets\products\images');
+		//$crud->set_field_upload('file','assets\products\files');
+		$this->mPageTitle = 'Products';
+		$this->render_crud();
+	}
+	public function cart()
+	{
+		$this->add_script('assets/dist/admin/jquery.mycart.js',TRUE, 'head');
+		$this->mViewData['products'] = $this->db->select('*')
+							->from('products')
+							->get()->result_array();
+		$this->render('cart');
+	}
+
+	public function add(){
+		$data = array(
+        		'user_id' => $this->input->post('user_id'),
+	        	'product_id' => $this->input->post('product_id'),
+        		'qty' => $this->input->post('qty')
+		);
+
+		$this->db->insert('orders', $data);
+		echo "done";
+	}
+}
